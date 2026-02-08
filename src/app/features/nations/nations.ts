5d22f0dda431f1e90b674a9e30600b4555add8ff
lapminus <cyoung35@calstatela.edu>
2026-02-08 13:42:52 -0800

    Implemented paging for players and nation

--- src/app/features/nations/nations.ts
+++ src/app/features/nations/nations.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { SearchComponent } from '../../shared/components/search/search';
+import { SearchService } from '../../shared/services/search';
+import { PlayerDto } from '../../shared/models/playerDto.model';
 
 @Component({
   selector: 'app-nations',
-  imports: [],
+  imports: [SearchComponent],
   templateUrl: './nations.html',
   styleUrl: './nations.scss',
 })
 export class Nations {
+  searchForNation = signal('Search nation...');
+  playerService = inject(SearchService);
+  players = signal<PlayerDto[]>([]);
 
+  onSearch(nation: string) {
+    this.playerService.search({ nation }).subscribe((pageResult) => {
+      this.players.set(pageResult.content);
+    });
+  }
 }
--- src/app/features/players/players.ts
+++ src/app/features/players/players.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { SearchComponent } from '../../shared/components/search/search';
+import { SearchService } from '../../shared/services/search';
+import { PlayerDto } from '../../shared/models/playerDto.model';
 
 @Component({
   selector: 'app-players',
-  imports: [],
+  imports: [SearchComponent],
   templateUrl: './players.html',
   styleUrl: './players.scss',
 })
 export class Players {
+  searchForName = signal('Search player...');
+  playerService = inject(SearchService);
+  players = signal<PlayerDto[]>([]);
 
+  onSearch(name: string) {
+    console.log(`PlayersComponent received: ${name}`);
+    this.playerService.search({ name }).subscribe((pageResult) => {
+      console.log('HTTP response:', JSON.stringify(pageResult, null, 2));
+      this.players.set(pageResult.content);
+    });
+  }
 }
