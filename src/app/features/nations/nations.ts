097314b7c3ce68033aa772a825abe0989409b471
lapminus <cyoung35@calstatela.edu>
2026-02-10 13:21:39 -0800

    Added nation flag styling

--- src/app/features/nations/nations.ts
+++ src/app/features/nations/nations.ts
@@ -3,6 +3,7 @@ import { SharedSearch } from '../../shared/components/shared-search/shared-searc
 import { PlayerService } from '../../shared/services/player.service';
 import { SharedNoResultsFound } from '../../shared/components/shared-no-results-found/shared-no-results-found';
 import { RouterLink } from '@angular/router';
+import { NATION_FLAG_MAP } from '../../shared/models/nation-flag.map';
 
 @Component({
   selector: 'app-nations',
@@ -32,5 +33,10 @@ export class Nations implements OnInit {
         return name?.toLowerCase().includes(nation.toLowerCase());
       }),
     );
+  }
+
+  getFlagUrl(nationCode: string): string {
+    const flagCode = NATION_FLAG_MAP[nationCode];
+    return flagCode ? `https://flagcdn.com/256x192/${flagCode}.png` : 'UNKNOWN-FLAG-CODE';
   }
 }
