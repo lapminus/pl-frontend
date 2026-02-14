a710a7bdb4dcf2c0e8cd5d194500f24a382d4aa0
lapminus <cyoung35@calstatela.edu>
2026-02-13 18:11:50 -0800

    Update pagination for teams to persist

--- src/app/features/teams/players-from-team/players-from-team.ts
+++ src/app/features/teams/players-from-team/players-from-team.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PlayerService } from '../../../shared/services/player.service';
 import { PlayerSummaryDto } from '../../../shared/models/playerSummaryDto.model';
 import { SharedNoResultsFound } from '../../../shared/components/shared-no-results-found/shared-no-results-found';
@@ -16,6 +16,7 @@ import { SharedPagination } from '../../../shared/components/shared-pagination/s
 export class PlayersFromTeam implements OnInit {
   playerService = inject(PlayerService);
   route = inject(ActivatedRoute);
+  router = inject(Router);
 
   isLoading = signal(true);
   hasError = signal(false);
@@ -27,28 +28,30 @@ export class PlayersFromTeam implements OnInit {
   sendingCurrentPage = signal<number>(0);
 
   ngOnInit(): void {
-    const team = this.route.snapshot.paramMap.get('teamName') ?? undefined;
-    this.sendingQuery.set(String(team));
+    this.route.queryParamMap.subscribe((params) => {
+      const page = Number(params.get('page'));
+      const team = this.route.snapshot.paramMap.get('teamName') ?? undefined;
+      this.sendingQuery.set(`${team}?page=${page}`);
 
-    this.playerService
-      .searchPlayersBy({ team })
-      .pipe(finalize(() => this.isLoading.set(false)))
-      .subscribe({
-        next: (result) => {
-          (this.sendingPlayers.set(result.content),
-            this.sendingPages.set(result.totalPages),
-            this.sendingCurrentPage.set(result.number));
-        },
-        error: () => this.hasError.set(true),
-      });
+      this.playerService
+        .searchPlayersBy({ team, page })
+        .pipe(finalize(() => this.isLoading.set(false)))
+        .subscribe({
+          next: (result) => {
+            (this.sendingPlayers.set(result.content),
+              this.sendingPages.set(result.totalPages),
+              this.sendingCurrentPage.set(result.number));
+          },
+          error: () => this.hasError.set(true),
+        });
+    });
   }
 
   receivePageChanged(page: number) {
-    this.playerService
-      .searchPlayersBy({ team: this.sendingQuery(), page })
-      .subscribe((pageResult) => {
-        this.sendingPlayers.set(pageResult.content);
-        this.sendingCurrentPage.set(pageResult.number);
-      });
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { page },
+      queryParamsHandling: 'replace',
+    });
   }
 }
